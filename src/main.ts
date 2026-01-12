ca29cdbca4ca39dbe10ff930cf021216ff7d7675
fabioferrari00 <fabioferrari.ff00@gmail.com>
2026-01-12 14:26:33 +0100

    milestone 3

--- src/main.ts
+++ src/main.ts
@@ -14,3 +14,43 @@ type Actress = Person & {
   awards: string,
   nationality: ActressNationality
 }
+
+function isActresses(dati: unknown): dati is Actress {
+  if (
+    dati &&
+    typeof dati === 'object' &&
+    "id" in dati &&
+    typeof dati.id === 'number' &&
+    "name" in dati &&
+    typeof dati.name === 'string' &&
+    "birth_year" in dati &&
+    typeof dati.birth_year === 'number' &&
+    "death_year" in dati &&
+    typeof dati.death_year === 'number' &&
+    "biography" in dati &&
+    typeof dati.biography === 'string' &&
+    "img" in dati &&
+    typeof dati.img === 'string'
+  ) {
+    return true;
+  }
+  return false
+}
+
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const res = await fetch(`http://localhost:3333/actresses/${id}`);
+    if (!res.ok) {
+      throw new Error(`Errore http ${res.status}: ${res.statusText}`)
+    }
+
+    const dati: unknown = await res.json()
+    if (!isActresses(dati)) {
+      throw new Error('Formato non valido')
+    }
+    return dati;
+  } catch (err) {
+    console.error(err)
+    return null
+  }
+}
